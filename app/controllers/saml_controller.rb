c15b6f930f78071ce68fabd075dc66dba39d8fd5
victorclee <victorchristopherlee@gmail.com>
2020-07-23 17:13:01 -0500

    Working on getting the metadata in the env to parse correctly.

---
+++ app/controllers/saml_controller.rb
@@ -0,0 +1,29 @@
+class SamlController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def init
+    request = OneLogin::RubySaml::Authrequest.new
+    redirect_to(request.create(saml_settings))
+  end
+
+  def consume
+    response = OneLogin::RubySaml::Response.new(params[:SAMLResponse])
+    request = OneLogin::RubySaml::Authrequest.new
+    response.settings = saml_settings
+    if response.is_valid?
+      session[:authenticated] = true
+      redirect_to root_path
+    else
+      redirect_to(request.create(saml_settings))
+    end
+  end
+
+  private
+  def saml_settings
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+    settings = idp_metadata_parser.parse(Rails.application.credentials.idp_metadata)
+    settings.assertion_consumer_service_url = "http://localhost:3000/saml/consume"
+    settings.issuer = 'http://localhost:3000/saml/consume'
+    settings
+  end
+
+end
