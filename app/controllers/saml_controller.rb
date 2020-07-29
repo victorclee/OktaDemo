7967a3fc375849f19c072d62ebdb661a0b425db8
victorclee <victorchristopherlee@gmail.com>
2020-07-29 15:13:35 -0500

    Successfully logged in with Okta. Removed debugger in saml controller.

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -3,7 +3,6 @@ class SamlController < ApplicationController
   def init
     request = OneLogin::RubySaml::Authrequest.new
     redirect_to(request.create(saml_settings))
-    debugger
   end
 
   def consume
@@ -15,8 +14,6 @@ class SamlController < ApplicationController
       redirect_to root_path
     else
       redirect_to(request.create(saml_settings))
-      debugger
-      
     end
   end
 
